ea5e68d23c9cc30dbf1f14c5af5c390087e2b3a4
HuyTCM1 <huytcm1@fsoft.com.vn>
2016-09-19 16:43:36 +0700

    Initial Commit

---
+++ Account/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Account
+//
+//  Created by HuyTCM1 on 9/19/16.
+//  Copyright © 2016 huytcm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Account/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Account
+//
+//  Created by HuyTCM1 on 9/19/16.
+//  Copyright © 2016 huytcm. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
